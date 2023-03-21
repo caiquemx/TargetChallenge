d75b0737c1b409ec6b1045c1026306d8874c55f3
Caique Moreira <caique_moreira_sz@hotmail.com>
2023-03-20 23:00:37 -0300

    fix: desafio 3

--- desafio3.js
+++ desafio3.js
@@ -16,3 +16,15 @@ e) 1, 1, 2, 3, 5, 8, ____
 f) 2,10, 12, 16, 17, 18, 19, ____
 
 */
+
+// resultados:
+
+/* a) próximo número é: 9 --- característica de uma PA crescente --- r = 2 ;
+   b) próximo número é: 128 --- característica de uma PG crescente --- q = 2;
+   c) próximo número é: 49 --- razão = a soma do primeiro termo com o primeiro natural
+   ímpar positivo e assim respectivamente. (0+1, 1+3, 4+5, 9+7, 16+9, 25+11, 36+13) .
+   d) próximo número é: 100 --- sequência de resultados do quadrado dos
+   naturais pares (2*2, 4*4, 6*6, 8*8, 10*10).
+   e) próximo número é: 13 --- sequência Fibonacci começando em 0 e 1.
+   f) próximo número é: inconclusivo;
+*/
